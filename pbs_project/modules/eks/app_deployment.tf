9830584642de6bbd332e2320679560213d7c24a2
kguskim <khkim010906@gmail.com>
2026-01-12 17:59:12 +0900

    22

--- pbs_project/modules/eks/app_deployment.tf
+++ pbs_project/modules/eks/app_deployment.tf
@@ -1,7 +1,7 @@
 # 1. 애플리케이션 Deployment (기차 만들기)
 resource "kubernetes_deployment" "pbs_app" {
   metadata {
-    name      = "pbs-app-deployment"
+    name      = "hybrid-ai-deployment"
     namespace = "default"
     labels = {
       app = "pbs-app"
@@ -28,6 +28,26 @@ resource "kubernetes_deployment" "pbs_app" {
         container {
           image = "198011705652.dkr.ecr.ap-northeast-2.amazonaws.com/hybrid-service:ai-latest" # 접속 시 예쁜 화면을 보여주는 이미지
           name  = "pbs-app"
+          env {
+            name  = "MILVUS_ADDR"
+            value = "milvus-service:19530" # Milvus 서비스 이름과 포트
+          }
+          env {
+            name  = "OLLAMA_ADDR"
+            value = "http://ollama-deployment:11434" # Ollama 서비스 이름과 포트
+          }
+
+          # [추가 추천] 자원 최적화 - t3.large 환경용
+          resources {
+            requests = {
+              cpu    = "100m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+          }
 
           image_pull_policy = "Always"
 
